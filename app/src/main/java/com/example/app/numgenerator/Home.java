271ceab493596050877f9ddee5fa178105ec2de8
xSallus <souza95salomao@gmail.com>
2020-01-20 20:23:57 -0400

    Some changes and additions to app

--- app/src/main/java/com/example/app/numgenerator/Home.java
+++ app/src/main/java/com/example/app/numgenerator/Home.java
@@ -3,12 +3,28 @@ package com.example.app.numgenerator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Home extends AppCompatActivity {
+
+    EditText range;
+    EditText nums;
+    EditText sets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        range = findViewById(R.id.Range);
+        sets = findViewById(R.id.NumOfSets);
+        nums = findViewById(R.id.NumsQuantity);
+    }
+
+    public void onClick(View view){
+        range.clearComposingText();
+        nums.clearComposingText();
+        sets.clearComposingText();
     }
 }
