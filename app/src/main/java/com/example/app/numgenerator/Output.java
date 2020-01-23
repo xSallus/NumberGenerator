2c6d5eb4ece8436a8e014fec32e3eb9beef99779
xSallus <souza95salomao@gmail.com>
2020-01-23 19:42:07 -0400

    some files modified/added

--- app/src/main/java/com/example/app/numgenerator/Output.java
+++ app/src/main/java/com/example/app/numgenerator/Output.java
@@ -1,13 +1,15 @@
 package com.example.app.numgenerator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.ParcelFormatException;
 import android.widget.LinearLayout;
-
-import java.text.ParseException;
+import android.widget.TextView;
 
 import regradenegocio.NGenerator;
 
@@ -23,15 +25,22 @@ public class Output extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output);
 
+        TextView setsOutput;
+
         Bundle extras = getIntent().getExtras();
         if(extras != null){
             range = extras.getString("range");
             nums = extras.getString("nums");
             sets = extras.getString("sets");
         }
-    }
 
-    public void handleData() {
-        //the data received from previous view will be treated here...
+        for (int i=0; i<Integer.parseInt(sets); i++) {
+            setsOutput = new TextView(this);
+            setsOutput.setId(i);
+            setsOutput.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            generator = new NGenerator(Integer.parseInt(nums), Integer.parseInt(range));
+            setsOutput.setText(generator.toString());
+            out.addView(setsOutput);
+        }
     }
 }
